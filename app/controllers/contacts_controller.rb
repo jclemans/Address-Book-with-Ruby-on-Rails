21a3026bb0e986c19ec4a5867ed242b716d84908
Jeremy Clemans & John Cech <student@epicodus.com>
2014-03-31 12:03:07 -0700

    consolidate restful routes and controller actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,4 +8,44 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params[:id])
     render('contacts/show.html.erb')
   end
+
+  def new
+    @contact = Contact.new
+    render('contacts/new.html.erb')
+  end
+
+  def create
+    @contact = Contact.new(:name => params[:name],
+                              :phone => params[:phone],
+                              :email => params[:email],
+                              :address => params[:address])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit.html.erb')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name],
+                       :phone => params[:phone],
+                       :email => params[:email],
+                       :address => params[:address])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render('contacts/destroy.html.erb')
+  end
 end
